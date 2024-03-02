b34f1014e362ead610f66113fafda68ab3700ea9
Paulamf23 <122610982+Paulamf23@users.noreply.github.com>
2024-03-02 16:30:08 +0100

    environment actualizacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,18 @@
+import 'firebase/compat/database';
+
 export const environment = {
-  production: true,
+  production: false,
   apiKey: '6d8db4c42263cde26c3490806b5b14ce',
   baseUrl: 'https://api.themoviedb.org/3',
   images: 'https://image.tmdb.org/t/p',
   firebaseConfig: {
     apiKey: 'AIzaSyBtv76fuAat5rZDxHrpY_BTUn78ccxNrvg',
     authDomain: 'filmplex-58afc.firebaseapp.com',
+    databaseURL: 'https://filmplex-58afc-default-rtdb.europe-west1.firebasedatabase.app',
     projectId: 'filmplex-58afc',
     storageBucket: 'filmplex-58afc.appspot.com',
     messagingSenderId: '31623827165',
     appId: '1:31623827165:web:c73c8016464e2b7e08d534'
   }
 };
+
